21db69ce37d52e9ec16604d04ac29aa8a99935b1
RAGHAV <eagle7@Raghavmac.local>
2024-08-21 19:00:31 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+def test_1(string =""): 
+	
+	substring = "" 
+	testList = [] 
+	initial = 0
+	
+	for char in string: 
+		
+		for i in range(initial, len(string)): 
+			substring+= string[i] 
+			
+		
+			if substring.count(string[i])>1: 
+				testList.append(substring[:-1]) 
+				initial+= 1
+				substring = "" 
+				break
+	maxi ="" 
+	
+	for word in testList: 
+		
+		if len(word)>len(maxi): 
+			maxi = word 
+			
+	if len(maxi)<3: 
+		return "-1"
+	else: 
+		return maxi 
+	
+# Driver code 
+print(test_1("character")) 
+print(test_1("standfan")) 
+print(test_1("class"))
